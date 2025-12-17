0fd85f7c540ca7c2912c48cce3a99f9e9b1e7a3b
Ade Irma Hamdadani <adehamdani1510@gmail.com>
2025-12-17 10:36:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,10 @@ Route::get('/', function () {
 // Route::get('/events', [EventController::class, 'index'])->name('events.index');
 // Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
 
-<<<<<<< HEAD
-// Test route - taruh di paling atas setelah use statements
-Route::get('/test-event-create', function() {
-    return 'Route berfungsi!';
-});
-=======
 // // Test route - taruh di paling atas setelah use statements
 // Route::get('/test-event-create', function() {
 //     return 'Route berfungsi!';
 // });
->>>>>>> irma
 
 /*
 |--------------------------------------------------------------------------
@@ -47,18 +40,9 @@ Route::middleware(['auth'])->group(function () {
         ->name('dashboard');
 
     // Event Management (Organizer Only)
-<<<<<<< HEAD
-    Route::middleware(['can:isOrganizer'])->group(function () {
-        Route::get('/events/create-event', [EventController::class, 'create'])->name('events.create-event');
-        Route::post('/events', [EventController::class, 'store'])->name('events.store');
-        Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
-        Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
-        Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
-=======
     Route::middleware(['auth'])->group(function () {
         Route::resource('events', EventController::class);
 
->>>>>>> irma
     });
 
     // Registration
